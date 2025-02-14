1b26adc04519b314f6f0ec436977ed079ab65345
YuJin Hong <150586358+dbwls99706@users.noreply.github.com>
2025-02-14 13:51:01 +0900

    [level 2] Title: 완전범죄, Time: 73.19 ms, Memory: 11.8 MB -BaekjoonHub

--- 프로그래머스/2/389480. 완전범죄/완전범죄.py
+++ 프로그래머스/2/389480. 완전범죄/완전범죄.py
@@ -1,15 +1,15 @@
 def solution(info, n, m):
     l = len(info)
     dp = [[[0] * m for _ in range(n)] for _ in range(l + 1)]
-    dp[0][0][0] = True
+    dp[0][0][0] = 1
     
     for i in range(1, l + 1):
         for j in range(n):
             for k in range(m):
                 if j >= info[i-1][0] and dp[i-1][j-info[i-1][0]][k]:
-                    dp[i][j][k] = True
+                    dp[i][j][k] = 1
                 elif k >= info[i-1][1] and dp[i-1][j][k-info[i-1][1]]:
-                    dp[i][j][k] = True
+                    dp[i][j][k] = 1
     
     for j in range(n):
         for k in range(m):
